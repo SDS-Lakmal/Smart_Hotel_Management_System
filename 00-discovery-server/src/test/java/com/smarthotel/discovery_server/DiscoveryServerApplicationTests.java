2e6b6ac1a7c53b2883be9d14be7e72dbcf3b1647
SDS-Lakmal <sanjulalakmal548@gmail.com>
2026-01-28 18:18:15 +0530

    chore: Initial setup

---
+++ 00-discovery-server/src/test/java/com/smarthotel/discovery_server/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarthotel.discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
