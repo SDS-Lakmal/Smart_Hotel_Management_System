e2d53bf92fe91fb789d71d06f99864392979e2d1
Pasindu Induwara <pasinduinduwarak19@gmail.com>
2026-03-05 17:45:46 +0530

    Remove_04-room-service_from_reservation_branch

--- 04-room-service/src/main/java/com/smarthotel/room_service/repository/RoomRepository.java
+++
@@ -1,8 +0,0 @@
-package com.smarthotel.room_service.repository;
-
-import com.smarthotel.room_service.model.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomRepository extends JpaRepository<Room, Long> {
-
-}
