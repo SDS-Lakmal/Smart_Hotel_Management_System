b573548ade00f15e417199d33365ac45d2fa76fb
SDS-Lakmal <sanjulalakmal548@gmail.com>
2026-03-06 00:25:45 +0530

    Fix: Restored deleted GuestController.java and fixed API endpoints

--- 99-frontend-client/.next/dev/server/server-reference-manifest.js
+++ 99-frontend-client/.next/dev/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"d7ry4ShHXPm+fk5nWUGltbz5NcxJBI2mk0foXaNJ8qs=\"\n}"
